0f9fbaab15e94b795b5c97b269abef3896992220
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2024-12-08 22:59:59 +0200

    added backend

---
+++ FitMetrics-Server/src/main/java/com/example/FitMetrics_Server/FitMetricsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FitMetrics_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMetricsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMetricsServerApplication.class, args);
+	}
+
+}
