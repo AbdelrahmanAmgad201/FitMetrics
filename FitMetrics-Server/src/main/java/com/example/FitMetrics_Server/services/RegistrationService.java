6941a8722ca56b17a8279520a93d3917c4cd3147
Ayman Algamal <ayman01287985950@gmail.com>
2024-12-11 15:09:19 +0200

    Hashing Password Done.

--- FitMetrics-Server/src/main/java/com/example/FitMetrics_Server/services/RegistrationService.java
+++ FitMetrics-Server/src/main/java/com/example/FitMetrics_Server/services/RegistrationService.java
@@ -3,6 +3,7 @@ package com.example.FitMetrics_Server.services;
 import com.example.FitMetrics_Server.entities.User;
 import com.example.FitMetrics_Server.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,18 +14,19 @@ public class RegistrationService {
     @Autowired
     private UserRepository userRepository;
 
+    // BCryptPasswordEncoder instance for hashing passwords
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-
-
-
+    // Register User with password hashing
     public Long registerUser(String userName, String password) throws Exception {
+        // Check if the user already exists
         if (userRepository.findByUserName(userName).isPresent()) {
             throw new Exception("User already exists");
         }
 
         User user = new User();
         user.setUserName(userName);
-        user.setHashedPassword(password);
+        user.setHashedPassword(passwordEncoder.encode(password)); // Hash the password before saving
         return userRepository.save(user).getId();
     }
 
@@ -47,17 +49,14 @@ public class RegistrationService {
         throw new RuntimeException("User not found");
     }
 
-
     public Long login(String userName, String password) {
         Optional<User> user = userRepository.findByUserName(userName);
-        if (user.isPresent() && user.get().getHashedPassword().equals(password)) {
-            return user.get().getId();
+
+      if (user.isPresent() && passwordEncoder.matches(password, user.get().getHashedPassword())) {
+            return user.get().getId();  // Return the user ID if password matches
         }
+
+        // Throw exception if invalid login
         throw new RuntimeException("Invalid username or password");
     }
-
-
-
-
 }
-
