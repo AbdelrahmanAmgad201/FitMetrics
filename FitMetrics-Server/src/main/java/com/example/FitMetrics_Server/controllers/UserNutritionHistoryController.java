eac3c54b6653e8190c85cb476c4f358c89e18575
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2024-12-14 05:32:36 +0200

    added JWT token to nutrition history

--- FitMetrics-Server/src/main/java/com/example/FitMetrics_Server/controllers/UserNutritionHistoryController.java
+++ FitMetrics-Server/src/main/java/com/example/FitMetrics_Server/controllers/UserNutritionHistoryController.java
@@ -2,7 +2,9 @@ package com.example.FitMetrics_Server.controllers;
 
 import com.example.FitMetrics_Server.entities.FoodData;
 import com.example.FitMetrics_Server.entities.UserNutritionHistory;
+import com.example.FitMetrics_Server.services.AuthService;
 import com.example.FitMetrics_Server.services.UserNutritionHistoryService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +17,20 @@ public class UserNutritionHistoryController {
     @Autowired
     private UserNutritionHistoryService userNutritionHistoryService;
 
-    @PostMapping("/add-food/{userId}")
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/add-food")
     public ResponseEntity<?> addFoodToHistory(
-            @PathVariable long userId,
+            @RequestHeader("Authorization") String token,
             @RequestBody Map<String, Object> foodJson
     ) {
         try {
+            // Extract user ID from JWT
+            token = token.replace("Bearer ", "");
+            Claims claims = authService.parseToken(token);
+            Long userId = Long.parseLong(claims.getId());
+
             // Convert JSON to FoodData
             FoodData foodData = userNutritionHistoryService.convertJsonToFoodData(foodJson);
 
@@ -33,12 +43,17 @@ public class UserNutritionHistoryController {
         }
     }
 
-    @DeleteMapping("/delete-food/{userId}")
+    @DeleteMapping("/delete-food")
     public ResponseEntity<?> deleteFoodEntryForToday(
-            @PathVariable Long userId,
+            @RequestHeader("Authorization") String token,
             @RequestParam Long foodId
     ) {
         try {
+            // Extract user ID from JWT
+            token = token.replace("Bearer ", "");
+            Claims claims = authService.parseToken(token);
+            Long userId = Long.parseLong(claims.getId());
+
             userNutritionHistoryService.deleteOneFoodEntryForToday(userId, foodId);
             return ResponseEntity.ok("Food entry deleted successfully");
         } catch (RuntimeException e) {
